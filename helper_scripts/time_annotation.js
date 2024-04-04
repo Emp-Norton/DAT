5b513719456bc8825b07346371b2fddaec05fdc5
James Jelenko <jamesjelenko@gmail.com>
2024-04-04 04:21:31 -0700

    [4/4/24] add comments and wrapper function for interval later

--- helper_scripts/time_annotation.js
+++ helper_scripts/time_annotation.js
@@ -1,4 +1,11 @@
-document.querySelectorAll('time').forEach((el) => {
-  let parent = el.parentNode;
-  parent.innerText = new Date(el.dateTime * 1).toLocaleTimeString();
-})
+
+const updateTimeAnnotation = () => {
+  // Select all elements of "time" tag and loop over them
+  document.querySelectorAll('time').forEach((el) => {
+    // select the parent node, should be a cell in the table with a relative-to-now timestamp
+    let parent = el.parentNode;
+    // set the innertext to a local time string for absolute time of submission. The multiply by 1 converts the unix timestamp from a string to int, otherwise it screams about invalid date.
+    parent.innerText = new Date(el.dateTime * 1).toLocaleTimeString();
+  })
+}
+
